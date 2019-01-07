16a906f6c80ec3bbf200f7e872153e277fdd2316
s.hitipeuw <werksimon@gmail.com>
2019-01-07 20:33:13 +0100

    Uitwerkingen.

--- WEEK3/5. Array-Set/script.js
+++ WEEK3/5. Array-Set/script.js
@@ -1,2 +1,17 @@
 "use strict";
 
+useArray();
+useSet();
+
+function useArray() {
+    let fruits = new Array('Apple', 'Banana', 'Mango', 'Banana');
+    main.showTitle("Using array (with duplicates)");
+    main.showArrayProperties(fruits);
+}
+
+function useSet() {
+    let fruitsSet = new Set(['Apple', 'Banana', 'Mango', 'Banana']);
+    let fruitsArray = Array.from(fruitsSet);
+    main.showTitle("Using set (no duplicates)");
+    main.showArrayProperties(fruitsArray);
+}
