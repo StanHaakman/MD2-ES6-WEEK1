c7e2af1891a5a2a4989958765af4f794d46bc621
Stan Haakman <stanhaakman@icloud.com>
2019-01-09 09:11:57 +0100

    Create script.js

---
+++ WEEK4/0.Intro/end/script.js
@@ -0,0 +1,32 @@
+"use strict";
+
+// Zo zou het kunnen
+
+function firstFunction() {
+   main.showTitle("Zo zou het kunnen");
+   let results = [];
+    	let celcius = [];
+   celcius[0] = 50;
+   result[0] = "Wanneer het buiten " + celcius[0] + " graden celcius is dan is dat " + ((celcius[0] * 1.8) + 32) + " fahrenheit."
+   main.showResult(result[0]);
+   celcius[1] = 51;
+   result[1] = "Wanneer het buiten " + celcius[1] + " graden celcius is dan is dat " + ((celcius[1] * 1.8) + 32) + " fahrenheit."
+   main.showResult(result[1]);
+   celcius[2] = 52;
+   result[2] = "Wanneer het buiten " + celcius[2] + " graden celcius is dan is dat " + ((celcius[2] * 1.8) + 32) + " fahrenheit."
+   main.showResult(result[2]);
+   celcius[3] = 53;
+   result[3] = "Wanneer het buiten " + celcius[3] + " graden celcius is dan is dat " + ((celcius[3] * 1.8) + 32) + " fahrenheit."
+   main.showResult(result[3]);
+   celcius[4] = 54;
+   result[4] = "Wanneer het buiten " + celcius[4] + " graden celcius is dan is dat " + ((celcius[4] * 1.8) + 32) + " fahrenheit." 
+
+   
+   main.showResult(result[4]);
+}
+
+
+firstFunction();
+
+
+// Maar het kan beter
